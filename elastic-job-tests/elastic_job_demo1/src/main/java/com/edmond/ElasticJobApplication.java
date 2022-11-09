2923a6952dae84dbdae2d2911ad9fe8fe1d634e5
liushuo <liushuo@glaway.com>
2022-11-09 17:25:15 +0800

    1.新增内容：etastic-job 整合 springboot 2. 实现的定时任务：固定时间通过日志输出任务参数

---
+++ elastic-job-tests/elastic_job_demo1/src/main/java/com/edmond/ElasticJobApplication.java
@@ -0,0 +1,18 @@
+/*
+ * 南京国睿信维软件有限公司
+ */
+package com.edmond;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author
+ * @date 2022/11/9
+ */
+@SpringBootApplication
+public class ElasticJobApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ElasticJobApplication.class, args);
+    }
+}
